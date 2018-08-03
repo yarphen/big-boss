f900500a6eb250ace2f06efa7160abd33536f614
yarphen <sheremet.mykhaylo@gmail.com>
2018-08-03 22:13:46 +0300

    Implement ACL

--- middlewares/acl.js
+++ middlewares/acl.js
@@ -1,18 +1,53 @@
+const { isSubordinate } = require('../services/boss');
+
+const asyncToMiddleware = asyncFunc => (req, res, next) => {
+  asyncFunc(req, res).then(() => next()).catch(err => next(err));
+};
+
 const checkAccessToUpdate = (req, res, next) => { // eslint-disable-line
-  // TODO implement with boss service
+  const { userId: currentUserId } = req.user || {};
+  const targetUserId = parseInt(req.params.userId, 10);
+  if (targetUserId !== currentUserId) {
+    next(new Error('Permission denied'));
+  }
   next();
 };
-const checkAccessToView = (req, res, next) => { // eslint-disable-line
-  // TODO implement with boss service
-  next();
+
+const checkAccessToView = async (req, res) => { // eslint-disable-line
+  const { userId: currentUserId } = req.user || {};
+  const targetUserId = parseInt(req.params.userId, 10);
+  if (targetUserId === currentUserId) {
+    return;
+  }
+  const isTargetUserSubordinateOfCurrent = await isSubordinate(targetUserId, currentUserId);
+  if (!isTargetUserSubordinateOfCurrent) {
+    throw new Error('Access denied');
+  }
 };
-const checkAccessToChangeBoss = (req, res, next) => { // eslint-disable-line
-  // TODO implement with boss service
-  next();
+
+const checkAccessToChangeBoss = async (req, res) => { // eslint-disable-line
+  const { userId: currentUserId } = req.user || {};
+  const { userId: targetUserId } = req.body;
+  const bossId = parseInt(req.params.userId, 10);
+  if (targetUserId === currentUserId || bossId === targetUserId) {
+    throw new Error('Access denied');
+  }
+  const isTargetUserSubordinateOfCurrent = isSubordinate(targetUserId, currentUserId);
+  if (!isTargetUserSubordinateOfCurrent) {
+    throw new Error('Access denied');
+  }
+  const isBossUserSubordinateOfCurrent = isSubordinate(bossId, currentUserId);
+  if (!isBossUserSubordinateOfCurrent && bossId !== currentUserId) {
+    throw new Error('Access denied');
+  }
+  const isBossUserSubordinateOfTarget = isSubordinate(bossId, targetUserId);
+  if (!isBossUserSubordinateOfTarget) {
+    throw new Error('Circular dependency is not allowed!');
+  }
 };
 
 module.exports = {
   checkAccessToUpdate,
-  checkAccessToView,
-  checkAccessToChangeBoss,
+  checkAccessToView: asyncToMiddleware(checkAccessToView),
+  checkAccessToChangeBoss: asyncToMiddleware(checkAccessToChangeBoss),
 };
