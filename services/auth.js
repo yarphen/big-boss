ec3376aa8f9d6f5679b68efb695ddb3497285753
yarphen <sheremet.mykhaylo@gmail.com>
2018-08-03 20:46:26 +0300

    Implementing boss service

--- services/auth.js
+++ services/auth.js
@@ -4,13 +4,23 @@ const { sendMail } = require('./email');
 const { models: { user } } = require('../models');
 const { makeHash, makeRandomPass } = require('../utils/password');
 
+const { ROLE_USER, ROLE_ROOT } = require('../constants');
+
 const signup = async (newUser) => {
   const passwordHash = makeHash(newUser.password);
   const userWithSameEmail = await user.findOne({ where: { email: newUser.email } });
   if (userWithSameEmail) {
     throw new Error('User with this email already exists');
   }
-  return user.create({ ...newUser, pass: passwordHash });
+  const rootUser = await user.findOne({ where: { roleId: ROLE_ROOT } });
+  let currentRole = ROLE_USER;
+  let bossId = null;
+  if (!rootUser) {
+    currentRole = ROLE_ROOT;
+  } else {
+    bossId = rootUser.userId;
+  }
+  return user.create({ ...newUser, pass: passwordHash, roleId: currentRole, bossId });
 };
 
 const reset = async (email) => {
